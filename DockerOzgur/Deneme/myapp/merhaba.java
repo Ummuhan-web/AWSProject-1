797dd4573365d96cf20f766ff0c86c5f762dfea3
UMMUHAN <umm.yvz@gmail.com>
2021-03-27 02:40:58 +0000

    Ozgr

---
+++ DockerOzgur/Deneme/myapp/merhaba.java
@@ -0,0 +1,7 @@
+public public class merhaba {
+    public static void main(String[] args) {
+        System.out.println("Merhaba arkadaslar. Ben merhaba isimli Java konsol uygulamasıyım");
+    }
+}class merhaba {
+    
+}
